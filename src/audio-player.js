c98235fcd0bf1fe26d6495f3b1bade48843ad874
kez <kerrialbeckettnewham@gmail.com>
2023-11-11 19:29:40 +0100

    add recoding functionality

--- src/audio-player.js
+++ src/audio-player.js
@@ -2,18 +2,22 @@ import {Controller} from '@hotwired/stimulus';
 import anime from 'animejs'
 import './main.css'
 
-
-import {mdiAccount} from '@mdi/js';
-
 export default class extends Controller {
 
+
+    audio = new Audio();
+    audioContext = null;
+    audioContextStarted = false;
+    mediaStream = null
+    mediaRecorder = null
+    audioChunks = [];
 
     static targets = [
         'player',
         'play',
+        'record',
         'seek',
         'time',
-        'visualiser',
         'rect',
         'circle'
     ]
@@ -21,39 +25,180 @@ export default class extends Controller {
     static values = {
         file: String,
         id: String,
+        isRecorder: {
+            type: Boolean,
+            default: false
+        },
         theme: {
             type: String,
             default: 'default'
         }
+
     }
 
+    initialize() {
+        this.generatePlayerElements()
+        this.resolveTheme();
+    }
 
+    async connect() {
+        if (this.isRecorderValue) {
+            this.mediaStream = await navigator.mediaDevices.getUserMedia({audio: true});
+            this.mediaRecorder = new MediaRecorder(this.mediaStream);
+        }
+
+        if (this.fileValue) {
+            this.audio.src = this.fileValue;
+            await this.setupAudioContext()
+            await this.loadAudio()
+        }
+    }
+
+    async setupAudioContext() {
+        if (this.audioContext) {
+            await this.audioContext.close();
+            this.audioContextStarted = false;
+        }
+
+        let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        this.audioContext = audioCtx;
+        this.gain = this.audioContext.createGain();
+        this.analyser = this.audioContext.createAnalyser();
+
+        if (!this.sourceNode) {
+            this.sourceNode = this.audioContext.createMediaElementSource(this.audio);
+            this.sourceNode
+                .connect(this.gain)
+                .connect(this.analyser)
+                .connect(this.audioContext.destination);
+        }
+    }
+
+    async record() {
+
+        if (!this.recording) {
+            this.audio = new Audio();
+            this.recordTarget.style.color = '#c62828';
+            await this.startRecording();
+        } else {
+            this.recordTarget.style.color = '#333';
+            await this.stopRecording();
+        }
+    }
+
+    async startRecording() {
+        // Check if the MediaRecorder API is available
+        if (typeof MediaRecorder === 'undefined') {
+            console.error('MediaRecorder API is not supported in this browser.');
+            return;
+        }
+
+        await this.audioContext.resume();
+
+        try {
+            return new Promise((resolve, reject) => {
+                // When data is available, add it to the chunks array
+                this.mediaRecorder.ondataavailable = (event) => {
+                    if (event.data.size > 0) {
+                        this.audioChunks.push(event.data);
+                    }
+                };
+
+                // When the recording stops, resolve the Promise
+                this.mediaRecorder.onstop = () => {
+                    if (this.audioChunks.length > 0) {
+                        this.recordedAudio = new Blob(this.audioChunks, {type: 'audio/wav'});
+                        console.log('Recording stopped, recordedAudio is set:', this.recordedAudio);
+                        resolve(this.recordedAudio);
+                    } else {
+                        console.error('No audio data recorded.');
+                        reject('No audio data recorded.');
+                    }
+                };
+
+                // Start recording
+                this.mediaRecorder.start();
+                this.recording = true;
+                console.log('Recording started.');
+            });
+        } catch (error) {
+            console.error('Error starting recording:', error);
+        }
+    }
+
+    async stopRecording() {
+        if (this.mediaRecorder && this.recording) {
+            try {
+                // Create a promise that resolves when the recording stops and the recorded audio is available.
+                const recordingPromise = new Promise((resolve, reject) => {
+                    this.mediaRecorder.onstop = async () => {
+                        if (this.audioChunks.length > 0) {
+                            this.recordedAudio = new Blob(this.audioChunks, {type: 'audio/wav'});
+                            console.log('Recording stopped, recordedAudio is set:', this.recordedAudio);
+                            resolve(this.recordedAudio);
+                        } else {
+                            console.error('No audio data recorded.');
+                            reject('No audio data recorded.');
+                        }
+                    };
+                });
+
+                // Stop the recording
+                this.mediaRecorder.stop();
+                this.mediaStream.getTracks().forEach((track) => track.stop());
+                this.recording = false;
+                console.log('Recording stopped.');
+
+                // Wait for the recordingPromise to resolve before continuing
+                await recordingPromise;
+
+                if (this.recordedAudio instanceof Blob) {
+                    this.audio.src = URL.createObjectURL(this.recordedAudio);
+
+                    // Set up a new audio context for the new audio element
+                    await this.setupAudioContext();
+                    await this.loadAudio();
+                } else {
+                    console.error('Recorded audio is not a valid Blob:', this.recordedAudio);
+                }
+            } catch (error) {
+                console.error('Error while stopping recording:', error);
+            }
+        }
+    }
+
+    async loadAudio() {
+        return new Promise((resolve) => {
+            this.audio = new Audio(this.audio.src); // Create a new Audio element
+
+            this.audio.onloadeddata = async () => {
+                this.timeTarget.textContent = this.fmtTime(this.audio.duration);
+                await this.setupAudioContext();
+
+                // Disconnect the previous source node if it exists and is connected
+                if (this.track) {
+                    this.track.disconnect();
+                }
+
+                // Create a new source node from the new audio element
+                this.track = this.audioContext.createMediaElementSource(this.audio);
+
+                // Connect the new source node
+                this.track.connect(this.gain);
+                this.track.connect(this.analyser);
+                this.track.connect(this.audioContext.destination);
+
+                this.updateVisualizer();
+                resolve();
+            };
+        });
+    }
     updateVisualizer() {
         this.graphic();
         requestAnimationFrame(() => this.updateVisualizer());
     }
 
-    initialize() {
-        this.generatePlayerElements()
-    }
-
-    connect() {
-        this.audio = new Audio(this.fileValue);
-        let audioCtx = window.AudioContext || window.webkitAudioContext;
-        this.audioContext = new audioCtx;
-        this.gain = this.audioContext.createGain();
-        this.analyser = this.audioContext.createAnalyser();
-        this.track = this.audioContext.createMediaElementSource(this.audio);
-        this.track
-            .connect(this.gain)
-            .connect(this.analyser)
-            .connect(this.audioContext.destination);
-
-        this.seek = this.seekTarget;
-        this.time = this.timeTarget;
-
-        console.log('AUDIO PLAYER CONNECTED!')
-        this.playTarget.setAttribute('data-isplaying', false)
+    resolveTheme() {
 
         this.fill = 'rgba(255,255,255,1)'
         const themes = ['light', 'default', 'dark'];
@@ -66,37 +211,48 @@ export default class extends Controller {
                 this.fill = '#2962ff'
             }
         }
-
-        this.audio.addEventListener("loadeddata", () => {
-            this.time.textContent = this.fmtTime(this.audio.duration);
-        });
-
-        this.audio.addEventListener("timeupdate", () => {
-            this.seek.value = this.audio.currentTime / this.audio.duration * 100;
-            this.time.textContent = this.fmtTime(this.audio.currentTime);
-        });
     }
 
-    toggle(event) {
+    async toggle(event) {
         if (this.audio.paused) {
-            this.playTarget.setAttribute('data-isplaying', true)
-            this.audioContext.resume()
-            this.playTarget.classList.remove('mdi-play')
-            this.playTarget.classList.add('mdi-pause')
-            this.audio.play()
-            this.updateVisualizer();
+            this.playTarget.setAttribute('data-isplaying', true);
+            // You can start the AudioContext when the audio is played
+            if (!this.audioContextStarted) {
+                await this.audioContext.resume();
+                this.audioContextStarted = true;
+            }
+            this.playTarget.classList.remove('mdi-play');
+            this.playTarget.classList.add('mdi-pause');
+            this.audio.play();
         } else {
-            this.playTarget.setAttribute('data-isplaying', false)
-            this.playTarget.classList.remove('mdi-pause')
-            this.playTarget.classList.add('mdi-play')
-            this.audio.pause()
+            this.playTarget.setAttribute('data-isplaying', false);
+            this.playTarget.classList.remove('mdi-pause');
+            this.playTarget.classList.add('mdi-play');
+            this.audio.pause();
         }
     }
 
-    seeking(event) {
-        const pct = this.seek.value / 100;
-        this.audio.currentTime = (this.audio.duration || 0) * pct;
+    seek(event) {
+        const pct = parseInt(this.seekTarget.value, 10) / 100;
+
+        if (!isNaN(pct) && isFinite(pct)) {
+            if (!isNaN(this.audio.duration) && isFinite(this.audio.duration)) {
+                const newTime = this.audio.duration * pct;
+                if (!isNaN(newTime) && isFinite(newTime)) {
+                    this.audio.currentTime = newTime;
+                } else {
+                    console.error('Invalid newTime:', newTime);
+                }
+            } else {
+                console.error('Invalid audio duration:', this.audio.duration);
+            }
+        } else {
+            console.error('Invalid seek percentage:', pct);
+        }
     }
+
+
+
 
     graphic() {
         let freqArray = new Uint8Array(this.analyser.frequencyBinCount);
@@ -172,7 +328,6 @@ export default class extends Controller {
         });
     }
 
-
     generatePlayerElements() {
         // Create the player container
         const playerContainer = document.createElement('div');
@@ -183,15 +338,29 @@ export default class extends Controller {
         const playButton = document.createElement('div');
         playButton.setAttribute('data-action', 'click->audio-player#toggle');
         playButton.setAttribute('data-audio-player-target', 'play');
+        playButton.setAttribute('data-isplaying', false)
         playButton.classList.add('_saw_play_btn');
         playButton.classList.add('mdi')
         playButton.classList.add('mdi-play')
         playButton.style.color = '#333'
         playerContainer.appendChild(playButton);
 
+
+        if (this.isRecorderValue) {
+            // Create the record button
+            const recordButton = document.createElement('div');
+            recordButton.setAttribute('data-action', 'click->audio-player#record');
+            recordButton.setAttribute('data-audio-player-target', 'record');
+            recordButton.classList.add('_saw_record_btn');
+            recordButton.classList.add('mdi')
+            recordButton.classList.add('mdi-record')
+            recordButton.style.color = '#333'
+            playerContainer.appendChild(recordButton);
+        }
+
         // Create the seek input
         const seekInput = document.createElement('input');
-        seekInput.setAttribute('data-action', 'change->audio-player#seeking');
+        seekInput.setAttribute('data-action', 'change->audio-player#seek');
         seekInput.setAttribute('data-audio-player-target', 'seek');
         seekInput.type = 'range';
         seekInput.step = '0.01';
